d35fc7d951c407d7cc3c86f9ee3379cd090f37de
Lakshya Kumar <kumarlakshya101@gmail.com>
2023-12-21 22:04:18 +0530

    formatted the code

--- src/components/ExploreExams.js
+++ src/components/ExploreExams.js
@@ -1,37 +1,50 @@
-import { ExamsDetails } from "../utils/Data"
+import { ExamsDetails } from "../utils/Data";
 import { useNavigate } from "react-router-dom";
 
 const ExploreExams = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleClick = (category) => {
-        navigate('/mcq',{ state: category  });
-    }
+  const handleClick = (category) => {
+    navigate("/mcq", { state: category });
+  };
 
-    return (
-        <div className=' w-full px-48 flex flex-col justify-start items-center gap-y-12 max-xl:px-20 max-xl:py-10 max-sm:px-10 max-xxs:px-4 bg-red-300 my-12'>
-            <div className='flex w-full justify-between items-center max-xxs:flex-col max-xxs:gap-y-4'>
-                <span className='text-[#1D4645] text-4xl font-bold capitalize max-md:text-3xl max-xxs:text-2xl max-xxs:text-center'>Explore Our Exam Categories</span>
-                <button className='cursor pointer border border-[#1D4645] p-3 max-xs:p-1 uppercase rounded-md w-36 flex justify-center items-center max-md:text-sm hover:bg-[#E8C5B0] hover:bg-opacity-30'>Explore All</button>
-            </div>
-            <div className='flex flex-wrap w-full gap-12 [&>*]:w-80 [&>*]:h-80  justify-between max-lg:gap-8 max-lg:[&>*]:w-[17.5rem] max-lg:[&>*]:h-[17.5rem]
-                            max-md:[&>*]:w-56 max-md:[&>*]:h-56 max-xs:[&>*]:w-full max-xs:[&>*]:h-48 '>
-                {ExamsDetails.map((exam) => {
-                    return (
-                        <div className={`${exam.bgColor} ${exam.textColor} px-3 rounded-md flex flex-col justify-center items-center p-2 gap-y-4 
+  return (
+    <div className=" w-full px-48 flex flex-col justify-start items-center gap-y-12 max-xl:px-20 max-xl:py-10 max-sm:px-10 max-xxs:px-4 bg-red-300 my-12">
+      <div className="flex w-full justify-between items-center max-xxs:flex-col max-xxs:gap-y-4">
+        <span className="text-[#1D4645] text-4xl font-bold capitalize max-md:text-3xl max-xxs:text-2xl max-xxs:text-center">
+          Explore Our Exam Categories
+        </span>
+        <button className="cursor pointer border border-[#1D4645] p-3 max-xs:p-1 uppercase rounded-md w-36 flex justify-center items-center max-md:text-sm hover:bg-[#E8C5B0] hover:bg-opacity-30">
+          Explore All
+        </button>
+      </div>
+      <div
+        className="flex flex-wrap w-full gap-12 [&>*]:w-80 [&>*]:h-80  justify-between max-lg:gap-8 max-lg:[&>*]:w-[17.5rem] max-lg:[&>*]:h-[17.5rem]
+                            max-md:[&>*]:w-56 max-md:[&>*]:h-56 max-xs:[&>*]:w-full max-xs:[&>*]:h-48 "
+      >
+        {ExamsDetails.map((exam) => {
+          return (
+            <div
+              className={`${exam.bgColor} ${exam.textColor} px-3 rounded-md flex flex-col justify-center items-center p-2 gap-y-4 
                                         shadow-xl shadow-gray-400  cursor-pointer hover:transition-transform hover:scale-[105%] hover:duration-300`}
-                        key={exam.name}
-                        onClick={()=> handleClick({category:exam.name})}
-                        >
-                            <img src={exam.image} alt="linux img" className='w-20 bg-white rounded-full'/>
-                            <div className='font-bold text-lg'>{exam.name}</div>
-                            <div className='font-extralight text-sm h-24 overflow-clip flex items-center justify-center max-md:hidden'>{exam.description}</div>
-                        </div>
-                    )
-                })}
+              key={exam.name}
+              onClick={() => handleClick({ category: exam.name })}
+            >
+              <img
+                src={exam.image}
+                alt="linux img"
+                className="w-20 bg-white rounded-full"
+              />
+              <div className="font-bold text-lg">{exam.name}</div>
+              <div className="font-extralight text-sm h-24 overflow-clip flex items-center justify-center max-md:hidden">
+                {exam.description}
+              </div>
             </div>
-        </div>
-    )
-}
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default ExploreExams
+export default ExploreExams;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,42 +2,48 @@ import { FaFacebookF } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 
-
-
 const Footer = () => {
-    return (
-        <div className='w-full h-[25rem] bg-[#1D4645] mt-10 px-48 flex flex-col items-center justify-center font-extralight gap-y-12
-                        max-xl:px-20 max-xl:py-10 max-sm:px-10 max-xs:text-sm max-xxs:px-4'>
-            <div className='flex justify-between items-center w-2/3 text-white max-lg:w-full'>
-                <div className='flex flex-col items-center justify-center gap-y-2'>
-                    <span className='text-[#E8C5B0] text-lg font-semibold'>Menu</span>
-                    <span>About</span>
-                    <span>Searvice</span>
-                    <span>Blog</span>
-                    <span>Content</span>
-                </div>
-                <div className='flex flex-col items-center justify-center gap-y-2'>
-                    <span className='text-[#E8C5B0] text-lg font-semibold'>Exams</span>
-                    <span>Exam 1</span>
-                    <span>Exam 2</span>
-                    <span>Exam 3</span>
-                    <span>Exam 4</span>
-                </div>
-                <div className='flex items-center justify-center gap-x-4 text-[#1D4645] max-xxs:flex-col max-xxs:gap-y-2'>
-                    <span className='bg-[#E8C5B0] w-12 h-12 max-md:w-8 max-md:h-8 rounded-full flex items-center justify-center'><FaFacebookF/></span>
-                    <span className='bg-[#E8C5B0] w-12 h-12 max-md:w-8 max-md:h-8 rounded-full flex items-center justify-center'><FaTwitter/></span>
-                    <span className='bg-[#E8C5B0] w-12 h-12 max-md:w-8 max-md:h-8 rounded-full flex items-center justify-center'><FaInstagram/></span>
-                </div>
-            </div>
-            <div className='text-white flex w-full justify-between items-center text-sm border-t py-12 max-md:text-xs max-xxs:flex-col max-xxs:gap-y-3 max-xxs:text-center'>
-                <div>Copyright © 2022 MCQ. All Rights Reserved.</div>
-                <div className="flex gap-x-4 ">
-                    <span>Privacy Policy</span>
-                    <span>Terms of Use</span>
-                </div>
-            </div>
+  return (
+    <div
+      className="w-full h-[25rem] bg-[#1D4645] mt-10 px-48 flex flex-col items-center justify-center font-extralight gap-y-12
+                        max-xl:px-20 max-xl:py-10 max-sm:px-10 max-xs:text-sm max-xxs:px-4"
+    >
+      <div className="flex justify-between items-center w-2/3 text-white max-lg:w-full">
+        <div className="flex flex-col items-center justify-center gap-y-2">
+          <span className="text-[#E8C5B0] text-lg font-semibold">Menu</span>
+          <span>About</span>
+          <span>Searvice</span>
+          <span>Blog</span>
+          <span>Content</span>
         </div>
-    )
-}
+        <div className="flex flex-col items-center justify-center gap-y-2">
+          <span className="text-[#E8C5B0] text-lg font-semibold">Exams</span>
+          <span>Exam 1</span>
+          <span>Exam 2</span>
+          <span>Exam 3</span>
+          <span>Exam 4</span>
+        </div>
+        <div className="flex items-center justify-center gap-x-4 text-[#1D4645] max-xxs:flex-col max-xxs:gap-y-2">
+          <span className="bg-[#E8C5B0] w-12 h-12 max-md:w-8 max-md:h-8 rounded-full flex items-center justify-center">
+            <FaFacebookF />
+          </span>
+          <span className="bg-[#E8C5B0] w-12 h-12 max-md:w-8 max-md:h-8 rounded-full flex items-center justify-center">
+            <FaTwitter />
+          </span>
+          <span className="bg-[#E8C5B0] w-12 h-12 max-md:w-8 max-md:h-8 rounded-full flex items-center justify-center">
+            <FaInstagram />
+          </span>
+        </div>
+      </div>
+      <div className="text-white flex w-full justify-between items-center text-sm border-t py-12 max-md:text-xs max-xxs:flex-col max-xxs:gap-y-3 max-xxs:text-center">
+        <div>Copyright © 2022 MCQ. All Rights Reserved.</div>
+        <div className="flex gap-x-4 ">
+          <span>Privacy Policy</span>
+          <span>Terms of Use</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
